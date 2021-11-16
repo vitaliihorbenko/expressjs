db0cf308131d34893e91a73e56eb9eb5a92e2403
Vitalii Horbnko <snaketsanders5@gmail.com>
2021-11-16 17:22:51 +0200

    Git innit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const homeRoutes = require("./routes/home");
+const coursesRoutes = require("./routes/courses");
+const addRoutes = require("./routes/add");
+
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use("/", homeRoutes);
+app.use("/courses", coursesRoutes);
+app.use("/add", addRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
