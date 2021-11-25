47550840dbe927002bafad25b40495865460b89a
Vitalii-Horbenko <62204874+Vitalii-Horbenko@users.noreply.github.com>
2021-11-25 21:57:02 +0200

    add userId

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById("619fbb427091beceaf35f1f5");
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log(e);
+  }
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", homeRoutes);
--- models/course.js
+++ models/course.js
@@ -10,6 +10,10 @@ const course = new Schema({
     required: true,
   },
   img: String,
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 module.exports = model("Course", course);
