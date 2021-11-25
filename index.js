75c674540ed292771f90c3419da47e1d159c4756
Vitalii Horbnko <snaketsanders5@gmail.com>
2021-11-25 18:48:15 +0200

    + MongoDB with user

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const path = require("path");
+const Handlebars = require("handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
 const homeRoutes = require("./routes/home");
 const cartRoutes = require("./routes/cart");
 const coursesRoutes = require("./routes/courses");
 const addRoutes = require("./routes/add");
+const User = require("./models/user");
 
 const app = express();
 
 const hbs = exphbs.create({
   defaultLayout: "main",
   extname: "hbs",
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
 });
 
 app.engine("hbs", hbs.engine);
@@ -30,8 +36,19 @@ const PORT = process.env.PORT || 3000;
 async function start() {
   try {
     const password = "Snake0241";
-    const url = `mongodb+srv://Vitalii:${password}@cluster0.4em9n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-    await mongoose.connect(url, { useNewUrlParser: true });
+    const url = `mongodb+srv://Vitalii:${password}@cluster0.4em9n.mongodb.net/nodeJsUdemy?retryWrites=true&w=majority`;
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+    });
+    const candidate = await User.findOne();
+    if (!candidate) {
+      const user = new User({
+        email: "punisher-07@mail.ru",
+        name: "Vitalii",
+        cart: { items: [] },
+      });
+      await user.save();
+    }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,7 @@ const Course = require("../models/course");
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const courses = await Course.getAll();
+  const courses = await Course.find();
   res.render("courses", {
     title: "Courses",
     isCourses: true,
@@ -16,20 +16,33 @@ router.get("/:id/edit", async (req, res) => {
     return res.redirect("/");
   }
 
-  const course = await Course.getById(req.params.id);
+  const course = await Course.findById(req.params.id);
   res.render("course-edit", {
     title: `Edit ${course.title}`,
     course,
   });
 
   router.post("/edit", async (req, res) => {
-    await Course.update(req.body);
+    const { id } = req.body;
+    delete req.body.id;
+    await Course.findOneAndUpdate(id, req.body);
     res.redirect("/courses");
   });
 });
 
+router.post("/remove", async (req, res) => {
+  try {
+    await Course.deleteOne({
+      _id: req.body.id,
+    });
+    res.redirect("/courses");
+  } catch (e) {
+    console.log(e);
+  }
+});
+
 router.get("/:id", async (req, res) => {
-  const course = await Course.getById(req.params.id);
+  const course = await Course.findById(req.params.id);
   res.render("course", {
     layout: "empty",
     title: `Course ${course.title}`,
